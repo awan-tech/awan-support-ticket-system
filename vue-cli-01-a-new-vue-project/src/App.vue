2e51eee0c4dbad9b427656e76b9e5783289e04ba
Larry Kuo <larrykuo1102@gmail.com>
2022-04-12 16:26:13 +0800

    header template-page insert

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,19 +1,37 @@
 <template>
-    <login-page></login-page>
+    <section>
+        <header-page></header-page>
+
+        
+        <section>
+            <login-page v-if="check_login_status()"></login-page>
+            <template-page v-else></template-page>
+        </section>
+        
+    </section>
+    
 </template>
 
 
 <script>
-import login from './components/login.vue'
 export default {
     props : {
 
     },
     data() {
-
+        return {
+            login : false,
+            role : ''
+        }
+        
     },
     methods : {
-
+        check_login_status() {
+            if ( this.login )
+                return true ;
+            else 
+                return false ;
+        }
     }
 }
 </script>
