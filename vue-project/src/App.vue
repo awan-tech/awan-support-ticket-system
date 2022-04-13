4540faff8010b68b17634d3fa684b2ea5bc113ce
Larry Kuo <larrykuo1102@gmail.com>
2022-04-13 11:58:01 +0800

    fix conflict and merge

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -15,6 +15,8 @@
 
 
 <script>
+// const axios = require('axios');
+
 export default {
     props : {
 
@@ -28,6 +30,38 @@ export default {
     },
     methods : {
         check_login_status() {
+
+        // axios.get('https://11931uvt3a.execute-api.us-east-2.amazonaws.com/test/helloworld')
+        // .then(function (response) {
+        //     // handle success
+        //     console.log(response);
+        // })
+        // .catch(function (error) {
+        //     // handle error
+        //     console.log(error);
+        // })
+        // .then(function (data ) {
+        //     console.log('test' + data )
+        //     // always executed
+        // });
+
+
+            fetch('https://11931uvt3a.execute-api.us-east-2.amazonaws.com/test/helloworld',{
+                method: 'GET',
+                mode : 'no-cors',
+                credentials: "include"
+                })
+            .then( (response) => {
+                if ( response.ok ) {
+                    return response.json() ;
+                }
+            })
+            .then((data) => {
+                
+                console.log( 'test' + data ) ;
+                    
+            })
+
             if ( this.login )
                 return true ;
             else 
